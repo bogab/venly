352af60fef01c38d938b1a461ddb9380895fdba6
bogab <bogdan.gabor@protonmail.com>
2023-09-19 12:59:16 +0300

    Task #2 - list entries

--- src/main/java/io/venly/words/controller/WordRelationController.java
+++ src/main/java/io/venly/words/controller/WordRelationController.java
@@ -4,10 +4,13 @@ import io.venly.words.dto.WordRelationDto;
 import io.venly.words.service.WordRelationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +23,12 @@ public class WordRelationController {
     public Long createWordRelation(@RequestBody @Valid WordRelationDto request) {
         var wordRelation = wordRelationService.createWordRelation(request);
         return wordRelation.getId();
+    }
+
+    @GetMapping
+    public List<WordRelationDto> getWordRelations() {
+        List<WordRelationDto> wordRelations = wordRelationService.getWordRelations();
+        return wordRelations;
     }
 
 }
--- src/main/java/io/venly/words/service/WordRelationService.java
+++ src/main/java/io/venly/words/service/WordRelationService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -25,5 +27,11 @@ public class WordRelationService {
                 .build();
 
         return wordRelationRepository.save(wordRelation);
+    }
+
+    public List<WordRelationDto> getWordRelations() {
+        return wordRelationRepository.findAll().stream()
+                .map(r -> new WordRelationDto(r.getWordOne(), r.getWordTwo(), r.getRelation()))
+                .toList();
     }
 }
