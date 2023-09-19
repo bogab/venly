1770228b1865361c86a55fb94d215fdce61d8b3e
bogab <bogdan.gabor@protonmail.com>
2023-09-19 13:20:45 +0300

    Task #5 - lowercase all words

--- src/main/java/io/venly/words/dto/WordRelationDto.java
+++ src/main/java/io/venly/words/dto/WordRelationDto.java
@@ -1,13 +1,17 @@
 package io.venly.words.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.venly.words.converter.ToLowerCaseConverter;
 import io.venly.words.entity.RelationType;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record WordRelationDto(
         @NotEmpty(message = "Word one is required")
+        @JsonDeserialize(converter = ToLowerCaseConverter.class)
         String wordOne,
         @NotEmpty(message = "Word two is required")
+        @JsonDeserialize(converter = ToLowerCaseConverter.class)
         String wordTwo,
         @NotNull(message = "Relation is required")
         RelationType relation,
